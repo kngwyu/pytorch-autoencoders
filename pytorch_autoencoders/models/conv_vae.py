3bd73fb0b5bdadf5c948ea65a9ec35f8de7ab7ca
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-01-03 12:22:20 +0900

    Use padding

--- pytorch_autoencoders/models/conv_vae.py
+++ pytorch_autoencoders/models/conv_vae.py
@@ -5,12 +5,12 @@ from ..config import Config
 from .vae import VariationalAutoEncoder
 
 
-def cnn_hidden(params: List[tuple], width: int, height: int) -> int:
-    for kernel, stride in params:
-        width = (width - kernel) // stride + 1
-        height = (height - kernel) // stride + 1
+def cnn_hidden(params: List[tuple], width: int, height: int) -> Tuple[int, int]:
+    for kernel, stride, padding in params:
+        width = (width - kernel + 2 * padding) // stride + 1
+        height = (height - kernel + 2 * padding) // stride + 1
     assert width > 0 and height > 0, 'Convolution makes dim < 0!!!'
-    return width * height
+    return width, height
 
 
 class ConvVae(VariationalAutoEncoder):
@@ -22,8 +22,7 @@ class ConvVae(VariationalAutoEncoder):
             input_dim: Size,
             config: Config,
             conv_channels: List[int] = [32, 32, 32, 32],
-            conv_args: List[tuple] = [(4, 2), (4, 2), (4, 2), (4, 2)],
-            deconv_args: List[tuple] = [(5, 2), (5, 2), (6, 2), (6, 2)],
+            conv_args: List[tuple] = [(4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1)],
             fc_units: List[int] = [256, 256],
             z_dim: int = 20,
             activator: nn.Module = nn.ReLU(True)
@@ -35,25 +34,29 @@ class ConvVae(VariationalAutoEncoder):
             (nn.Conv2d(channels[i], channels[i + 1], *conv_args[i]), activator)
             for i in range(len(channels) - 1)
         ]))
-        hidden = cnn_hidden(conv_args, *input_dim[-2:])
-        encoder_units = [hidden * channels[-1]] + fc_units
+        self.cnn_hidden = cnn_hidden(conv_args, *input_dim[-2:])
+        hidden = self.cnn_hidden[0] * self.cnn_hidden[1] * channels[-1]
+        encoder_units = [hidden] + fc_units
         self.encoder_fc = nn.Sequential(*chain.from_iterable([
             (nn.Linear(encoder_units[i], encoder_units[i + 1]), activator)
             for i in range(len(encoder_units) - 1)
         ]))
         self.mu_fc = nn.Linear(encoder_units[-1], z_dim)
         self.logvar_fc = nn.Linear(encoder_units[-1], z_dim)
-        decoder_units = [z_dim] + list(reversed(fc_units))
+        decoder_units = [z_dim] + list(reversed(fc_units[:-1])) + [hidden]
         self.decoder_fc = nn.Sequential(*chain.from_iterable([
             (nn.Linear(decoder_units[i], decoder_units[i + 1]), activator)
             for i in range(len(decoder_units) - 1)
         ]))
-        channels = [decoder_units[-1]] + list(reversed(channels[:-1]))
-        self.decoder_deconv = nn.Sequential(*chain.from_iterable([
-            (nn.ConvTranspose2d(channels[i], channels[i + 1], *deconv_args[i]), activator)
-            for i in range(len(channels) - 1)
-        ]))
-        del self.decoder_deconv[-1]
+        channels = list(reversed(conv_channels))
+        deconv = chain.from_iterable([(
+            nn.ConvTranspose2d(channels[i], channels[i + 1], *conv_args[i]),
+            activator
+        ) for i in range(len(channels) - 1)])
+        self.decoder_deconv = nn.Sequential(
+            *deconv,
+            nn.ConvTranspose2d(channels[-1], in_channel, *conv_args[-1])
+        )
         self.z_dim = z_dim
         self.to(config.device)
         config.initializer(self)
@@ -66,7 +69,7 @@ class ConvVae(VariationalAutoEncoder):
 
     def decode(self, z: Tensor, old_shape: Size = None) -> Tensor:
         h3 = self.decoder_fc(z)
-        h3 = h3.view(*h3.shape, 1, 1)
+        h3 = h3.view(h3.size(0), -1, *self.cnn_hidden)
         return self.decoder_deconv(h3)
 
 
@@ -77,8 +80,7 @@ def betavae_chairs(input_dim: Size, config: Config) -> ConvVae:
         input_dim,
         config,
         conv_channels=[32, 32, 64, 64],
-        conv_args=[(4, 2), (4, 2), (4, 2), (4, 2)],
-        deconv_args=[(5, 2), (5, 2), (6, 2), (6, 2)],
+        conv_args=[(4, 2, 1), (4, 2, 1), (4, 2, 1), (4, 2, 1)],
         fc_units=[256],
         z_dim=32,
     )
